1c4891a672cf0091ca775d0885448407d7a29f7a
Aap Vare <aap.vare@tudeng.ut.ee>
2021-10-06 15:19:16 +0300

    added project files

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    //public Text speedStr;
+    [SerializeField] private Text speedStr;
+    private int speed;
+    [SerializeField] private Rigidbody vehicle;
+
+    public void TextChange()
+    {
+        speedStr.text = speed.ToString();
+    }
+
+    private void Update()
+    {
+        var vel = vehicle.velocity;
+        speed = (int) (vel.magnitude * (10f/36f));
+        TextChange();
+    }
+}
